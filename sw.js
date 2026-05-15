58c79a60a53d9186ed930d639261a48f123cf757
bancelamine50-max <bancelamine50@gmail.com>
2026-05-15 23:07:22 +0200

    sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'memoire-pro-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './logo-transparent.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
